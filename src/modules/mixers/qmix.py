07e387278e246f5d5ec9b8d37605470b77d2b4c0
hqleeUstc <hqleee@outlook.com>
2022-11-06 01:26:53 +0800

    将icnn初步改造为qmix的格式

--- src/modules/mixers/qmix.py
+++ src/modules/mixers/qmix.py
@@ -36,6 +36,10 @@ class QMixer(nn.Module):
         self.rank = 1   #rank
         self.in_dim = args.n_agents
         self.n_agents = args.n_agents
+
+        self.state_dim = int(np.prod(args.state_shape))
+
+        self.embed_dim = args.mixing_embed_dim
         
         self.quadratic_layers = nn.ModuleList([
             nn.Sequential(
@@ -54,47 +58,80 @@ class QMixer(nn.Module):
             for (in_features, out_features) in sizes
         ])
 
-        self.final_layer = nn.Linear(hidden_layer_sizes[-1], 1, bias=True)
+        self.final_layer = nn.Linear(self.hidden_layer_sizes[-1], 1, bias=True)
         
-        hypernet_embed = 64     # QMIX的hypernet参数的中间channel数是64         self.args.hypernet_embed
-        self.hyper_w_1 = nn.Sequential(nn.Linear(64, hypernet_embed),
+        hypernet_embed = self.args.hypernet_embed     # QMIX的hypernet参数的中间channel数是64         self.args.hypernet_embed
+        self.hyper_w_1 = nn.Sequential(nn.Linear(self.state_dim, hypernet_embed),
                                         nn.ReLU(inplace=True),
-                                        nn.Linear(hypernet_embed, 32 * self.hidden_layer_sizes[1]))
-        self.hyper_w_2 = nn.Sequential(nn.Linear(64, hypernet_embed),
+                                        nn.Linear(hypernet_embed, self.embed_dim * self.n_agents))
+        self.hyper_w_2 = nn.Sequential(nn.Linear(self.state_dim, hypernet_embed),
                                         nn.ReLU(inplace=True),
-                                        nn.Linear(hypernet_embed, self.hidden_layer_sizes[1] * self.hidden_layer_sizes[2]))
+                                        nn.Linear(hypernet_embed, self.embed_dim * self.embed_dim))
    
-        self.hyper_w_final = nn.Sequential(nn.Linear(64, hypernet_embed),
+        self.hyper_w_final = nn.Sequential(nn.Linear(self.state_dim, hypernet_embed),
                                         nn.ReLU(inplace=True),
-                                        nn.Linear(hypernet_embed, self.hidden_layer_sizes[2] * 1))
+                                        nn.Linear(hypernet_embed, self.embed_dim))
 
         # State dependent bias for hidden layer
-        self.hyper_b_1 = nn.Linear(64, 32 * 2000)
-        self.hyper_b_2 = nn.Linear(64, 32 * 2000)
-        self.hyper_b_final = nn.Linear(64, 2000)
+        self.hyper_b_1 = nn.Linear(self.state_dim, self.embed_dim)
+        self.hyper_b_2 = nn.Linear(self.state_dim, self.embed_dim)
+        self.hyper_b_final = nn.Linear(self.state_dim, 2000)
+
+        # V(s) instead of a bias for the last layers
+        self.V = nn.Sequential(nn.Linear(self.state_dim, self.embed_dim),
+                               nn.ReLU(inplace=True),
+                               nn.Linear(self.embed_dim, 1))
                                         
-
-    def forward(self, input, states):
-        output = self.quadratic_layers[0](input)
-
-        # output_for_hyper = output.reshape(-1, 32, 1)
+    def forward(self, agent_qs, states):
+        bs = agent_qs.size(0)
+        states = states.reshape(-1, self.state_dim)
+        agent_qs = agent_qs.reshape(-1, 1, self.n_agents)
+        # First layer
+        w1 = self.hyper_w_1(states).abs() #if self.abs else self.hyper_w_1(states)
+        b1 = self.hyper_b_1(states)
+        w1 = w1.view(-1, self.n_agents, self.embed_dim)
+        b1 = b1.view(-1, 1, self.embed_dim)
+        hidden = F.elu(th.bmm(agent_qs, w1) + b1)
         
-        # for quadratic_layer in self.quadratic_layers[1:]:
+        # Second layer
+        w_final = self.hyper_w_final(states).abs() #if self.abs else self.hyper_w_final(states)
+        w_final = w_final.view(-1, self.embed_dim, 1)
+        # State-dependent bias
+        v = self.V(states).view(-1, 1, 1)
+        # Compute final output
+        y = th.bmm(hidden, w_final) + v
+        # Reshape and return
+        q_tot = y.view(bs, -1, 1)
+        
+        return q_tot
 
-        quad1 = self.quadratic_layers[1]
-        a1 = quad1(input)
+    def forward(self, agent_qs, states):
+        bs = agent_qs.size(0)
+        states = states.reshape(-1, self.state_dim)
+        # agent_qs = agent_qs.reshape(-1, 1, self.n_agents)
+
+        output = self.quadratic_layers[0](agent_qs)
+        agent_qs = agent_qs.reshape(-1, 1, self.n_agents)
+        output = output.reshape(-1, 1, self.n_agents)
+
+         # First layer
+        # w1 = self.hyper_w_1(states).abs() if self.abs else self.hyper_w_1(states)
+        # b1 = self.hyper_b_1(states)
+        # w1 = w1.view(-1, self.n_agents, self.embed_dim)
+        # b1 = b1.view(-1, 1, self.embed_dim)
+        # hidden = F.elu(th.bmm(agent_qs, w1) + b1)
+
 
         # First Layer, weights positive
-
-
         w1 = self.hyper_w_1(states).abs() # 暂时默认需要正的weights     if self.abs else self.hyper_w_1(states)
         b1 = self.hyper_b_1(states)
-        w1 = w1.view(32, 32)
-        b1 = b1.view(-1, 32)
-        hidden1 = F.elu(th.mm(output, w1) + b1) 
-        quad1 = self.quadratic_layers[1]
-        a1 = quad1(input)
-        output = hidden1 + quad1(input)
+        w1 = w1.view(-1, self.n_agents, self.embed_dim)
+        b1 = b1.view(-1, 1, self.embed_dim)
+        hidden1 = F.elu(th.bmm(output, w1) + b1)
+        quad1 = self.quadratic_layers[1](agent_qs)
+        a1 = quad1(agent_qs)
+        print(a1.shape)
+        output = hidden1 + quad1
         if self.activation == 'celu':
             output = th.celu(output)
         elif self.activation == 'softplus':
@@ -103,14 +140,26 @@ class QMixer(nn.Module):
             raise Exception('Activation is not specified or unknown.')
 
 
+       
+
+        # Second Layer
+        w_final = self.hyper_w_final(states).abs() #if self.abs else self.hyper_w_final(states)
+        w_final = w_final.view(-1, self.embed_dim, 1)
+        # State-dependent bias
+        v = self.V(states).view(-1, 1, 1)
+        # Compute final output
+        y = th.bmm(hidden1, w_final) + v
+        # Reshape and return
+        q_tot = y.view(bs, -1, 1)
+
         # Second Layer, weights positive
         w2 = self.hyper_w_2(states).abs() # 暂时默认需要正的weights     if self.abs else self.hyper_w_1(states)
         b2 = self.hyper_b_1(states)
-        w2 = w2.view(32, 32)
-        b2 = b2.view(-1, 32)
+        w2 = w2.view(-1, self.embed_dim, self.embed_dim)
+        b2 = b2.view(-1, 1, self.embed_dim)
         hidden2 = F.elu(th.mm(output, w2) + b2) 
-        quad2 = self.quadratic_layers[2]
-        output = hidden2 + quad2(input)
+        quad2 = self.quadratic_layers[2](input)
+        output = hidden2 + quad2
         if self.activation == 'celu':
             output = th.celu(output)
 
@@ -118,11 +167,12 @@ class QMixer(nn.Module):
         # Final, weights negative
         w_final = -self.hyper_w_final(states).abs() # 暂时默认需要正的weights     if self.abs else self.hyper_w_1(states)
         b_final = self.hyper_b_final(states)
-        w_final = w_final.view(32, -1)
-        b_final = b_final.view(-1, 1)
+        w_final = w_final.view(-1, self.embed_dim, 1)
+        b_final = b_final.view(-1, 1, 1)
         hidden_final = th.mm(output, w_final) + b_final
         output = hidden_final + .5 * self.strong_convexity * (input ** 2).sum(dim=1).reshape(-1, 1)
-        return output
+        q_tot = output.view(bs, -1, 1)
+        return q_tot
    
     def push(self, input):
         output = autograd.grad(
